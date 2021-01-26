5d3c4de344378d3da0aa467208a92219654de423
754914142 <754914142@qq.com>
2021-01-26 19:51:39 +0800

    1

--- app01/urls.py
+++ app01/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,include
 from app01 import views
 urlpatterns = [
     path('',views.index),
+    path('add_book/',views.add_book),
+
 ]
--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render,HttpResponse
+from app01 import models
 #https://www.runoob.com/django/django-orm-1.html
 # Create your views here.
 def index(request):
    return HttpResponse('OS')
+
+def add_book(request):
+   books = models.Book.objects.filter(price__lt=210)
+
+   print(books)
+   return HttpResponse('os')
+
